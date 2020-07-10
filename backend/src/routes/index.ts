312032096e7683b38491dfe2cd2c1db0b715a12d
regisfaria <rgisfaria@yahoo.com.br>
2020-07-10 16:56:09 -0300

    added 2 new routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
 import registerRouter from './register.routes';
+import userRouter from './user.routes';
+import leadRouter from './lead.routes';
 
 const routes = Router();
 
 routes.use('/register', registerRouter);
+routes.use('/users', userRouter);
+routes.use('/leads', leadRouter);
 
 export default routes;
