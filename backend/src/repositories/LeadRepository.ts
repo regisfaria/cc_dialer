14ece1b85cb208d0552d33784ba3a60dadffc490
regisfaria <rgsifaria@yahoo.com.br>
2020-07-17 22:51:27 -0300

    removed this files

--- backend/src/repositories/LeadRepository.ts
+++
@@ -1,6 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-
-import Lead from '../models/Lead';
-
-@EntityRepository(Lead)
-export default class LeadRepository extends Repository<Lead> {}
--- backend/src/repositories/ProfileRepository.ts
+++
@@ -1,6 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-
-import Profile from '../models/Profile';
-
-@EntityRepository(Profile)
-export default class ProfileRepository extends Repository<Profile> {}
