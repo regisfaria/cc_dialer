c81263b037439b7b70d131e598a158fa039b8aff
regisfaria <rgisfaria@yahoo.com.br>
2020-07-20 13:01:46 -0300

    position fix

--- frontend/src/components/SearchBar/index.tsx
+++ frontend/src/components/SearchBar/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { FiSearch } from 'react-icons/fi';
+
+import { SearchDiv } from './styles';
+
+/*
+ * I must set the search bar to search diferent things based on which
+ * page we're at
+ * Placeholder must change and so the search content
+ * Learn how to add parameters on components
+ */
 
 const SearchBar: React.FC = () => {
   return (
-    <>
-      <h1>Hello guys</h1>
-      <h2>Yoyoyo</h2>
-    </>
+    <SearchDiv>
+      <input type="text" placeholder="Global search" />
+      <button type="submit">
+        <FiSearch size={20} />
+      </button>
+    </SearchDiv>
   );
 };
 
